1d25ea60f6ff1e7eb569cef58a112c72ec053463
Mete Oezalp <mete.oezalp@icloud.com>
2022-12-11 17:36:09 +0100

    Day4_1

---
+++ Day4_1.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+
+public class Day4_1 {
+    public static void main(String args[]) throws IOException {  //static method
+        String input = Files.readString(Path.of("InputDay4.txt"));
+        
+        int score = 0;
+        String[] inputarray = input.split("\n");
+        String[] tmp;
+        String[] first;
+        String[] second;
+
+        for (String a :
+                inputarray) {
+            tmp = a.split(",");
+
+            first = tmp[0].split("-");
+            second = tmp[1].split("-");
+            if(StringToInt(first[0])>=StringToInt(second[0])
+                    && StringToInt(first[1])<= StringToInt(second[1])){
+                score++;
+            } else if (StringToInt(first[0])<=StringToInt(second[0])
+                    && StringToInt(first[1])>= StringToInt(second[1])) {
+                score++;
+
+            }
+        }
+        System.out.println(score);
+
+    }
+
+    private static int StringToInt(String number){
+        return Integer.parseInt(number.replaceAll("[^a-zA-Z0-9]", ""));
+    }
+}
